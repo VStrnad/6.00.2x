a2c42dae07efcf83419f3607e502e2af4793371e
andrewmarmion <andrew.marmion@gmail.com>
2015-11-04 14:57:12 -0800

    Completed Lecture 5 problem 1

--- Lecture 5/L5_1.py
+++ Lecture 5/L5_1.py
@@ -1,3 +1,5 @@
+import random
+
 def noReplacementSimulation(numTrials):
     '''
     Runs numTrials trials of a Monte Carlo simulation
@@ -6,6 +8,22 @@ def noReplacementSimulation(numTrials):
     drawn. Returns the a decimal - the fraction of times 3 
     balls of the same color were drawn.
     '''
-
+    count = 0
+    for n in range(numTrials):
+        balls = ["r", "r", "r", "g", "g", "g"]
+        i =0
+        counter = []
+        while i < 3:
+            choice = random.choice(balls)
+            balls.remove(choice)
+            counter.append(choice)
+            i += 1
+        if counter == ["r","r","r"] or counter == ["g", "g", "g"]:
+            count += 1
+    return float(count)/numTrials
+        
+print noReplacementSimulation(5000)
+            
+        
         
         
